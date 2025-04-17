a886deddd02c15ec6c3f0059247f793a83dd5a7d
Ginaries <155573435+Ginaries@users.noreply.github.com>
2025-04-17 00:40:58 -0300

    Agregue el Proyecto de Godot
    en la carpeta non escape esta el proyecto de godot

---
+++ non-escape/Scene/players/player.gd
@@ -0,0 +1,18 @@
+extends Area2D
+
+@export var Velocidad:int=500
+
+func _process(delta: float) -> void:
+	#-------Movimiento-----------
+	var velocity = Vector2.ZERO # The player's movement vector.
+	if Input.is_action_pressed("ui_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("ui_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("ui_down"):
+		velocity.y += 1
+	if Input.is_action_pressed("ui_up"):
+		velocity.y -= 1
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * Velocidad
+	position += velocity * delta
